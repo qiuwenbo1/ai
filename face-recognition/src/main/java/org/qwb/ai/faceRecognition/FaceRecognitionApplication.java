9f702087feacb3f36c5b04b418a2ae0eb4dce1fe
qiuwenbo1 <850854726@qq.com>
2023-11-28 13:51:10 +0800

    init project

---
+++ face-recognition/src/main/java/org/qwb/ai/faceRecognition/FaceRecognitionApplication.java
@@ -0,0 +1,13 @@
+package org.qwb.ai.faceRecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceRecognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceRecognitionApplication.class, args);
+    }
+
+}
